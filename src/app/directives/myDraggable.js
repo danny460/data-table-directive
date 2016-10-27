58f95eecdacb090da1e02120fee46aadf2cf74df
danny460 <Danielzlh62@gmail.com>
2016-10-28 00:08:39 +0800

    test draggable component

--- src/app/directives/myDraggable.js
+++ src/app/directives/myDraggable.js
@@ -1,25 +1,47 @@
-angular.module("test",[]).directive("myDraggable",myDraggable);
+angular.module("test", []).directive("myDraggable", myDraggable);
 
 myDraggable.$inject = ["$document"];
 
 //injecting jqlite service $document for DOM manipulation
-function myDraggable($document){
-	return link;
+function myDraggable($document) {
+    return link;
 
-	function link(scope, element, attr){
-		var startX = 0, startY = 0, x = 0, y = 0;
-	}	
+    function link(scope, element, attr) {
+        var startX = 0,
+            startY = 0,
+            x = 0,
+            y = 0;
 
-	element.css({
-		position: 'relative',
-		border: '1px solid yellow',
-		backgroundColor: 'purple',
-		cursor: 'pointer'
-	});
 
-	element.on('mousedown', function(event){
-		event.preventDefault();
-		start
-	});
+        element.css({
+            position: 'relative',
+            border: '5px solid black',
+            backgroundColor: 'red',
+            cursor: 'pointer'
+        });
+
+        element.on('mousedown', function(event) {
+            event.preventDefault();
+            startX = event.pageX - x;
+            startY = event.pageY - y;
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+            x = event.pageX - startX;
+            y = event.pageY - startY;
+            element.css({
+                top: y + 'px',
+                left: x + 'px'
+            });
+        }
+
+        function mouseup(event) {
+            $document.off('mousemove', mousemove);
+            $document.off('mouseup', mouseup);
+        }
+    }
+
 
 }
